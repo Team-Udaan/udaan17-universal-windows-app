861c5c579d44d6e2c0f165878eec602a60fad737
Dhruv Suthar <dhruvsuthar17@live.com>
2017-03-04 15:50:10 +0530

    Static Prototype commit

--- udaan17-universal-windows-app/udaan17-universal-windows-app.Windows/ItemPage.xaml.cs
+++ udaan17-universal-windows-app/udaan17-universal-windows-app.Windows/ItemPage.xaml.cs
@@ -17,13 +17,8 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-// The Universal Hub Application project template is documented at http://go.microsoft.com/fwlink/?LinkID=391955
-
 namespace udaan17_universal_windows_app
 {
-    /// <summary>
-    /// A page that displays details for a single item within a group.
-    /// </summary>
     public sealed partial class ItemPage : Page
     {
         private NavigationHelper navigationHelper;
@@ -35,37 +30,16 @@ namespace udaan17_universal_windows_app
             this.navigationHelper = new NavigationHelper(this);
             this.navigationHelper.LoadState += this.NavigationHelper_LoadState;
         }
-
-        /// <summary>
-        /// Gets the NavigationHelper used to aid in navigation and process lifetime management.
-        /// </summary>
         public NavigationHelper NavigationHelper
         {
             get { return this.navigationHelper; }
         }
-
-        /// <summary>
-        /// Gets the DefaultViewModel. This can be changed to a strongly typed view model.
-        /// </summary>
         public ObservableDictionary DefaultViewModel
         {
             get { return this.defaultViewModel; }
         }
-
-        /// <summary>
-        /// Populates the page with content passed during navigation.  Any saved state is also
-        /// provided when recreating a page from a prior session.
-        /// </summary>
-        /// <param name="sender">
-        /// The source of the event; typically <see cref="NavigationHelper"/>
-        /// </param>
-        /// <param name="e">Event data that provides both the navigation parameter passed to
-        /// <see cref="Frame.Navigate(Type, object)"/> when this page was initially requested and
-        /// a dictionary of state preserved by this page during an earlier
-        /// session.  The state will be null the first time a page is visited.</param>
         private async void NavigationHelper_LoadState(object sender, LoadStateEventArgs e)
         {
-            // TODO: Create an appropriate data model for your problem domain to replace the sample data
             var item = await DataSource.GetEventAsync((string)e.NavigationParameter);
             this.DefaultViewModel["Item"] = item;
         }
